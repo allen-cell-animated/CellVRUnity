ef9a6c0be29d61a71d4dd26174a7197ea2762e85
Blair Lyons <blair208@gmail.com>
2017-04-24 16:50:23 -0700

    moved build back to subdirectory, added mouse rotate and zoom controls

---
+++ Assets/Scripts/CameraPush.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPush : MonoBehaviour 
+{
+	public float minZ = -10f;
+	public float maxZ = -1.01f;
+	public float speed = 0.1f;
+
+	void Update () 
+	{
+		float zoom = transform.position.z + speed * Input.mouseScrollDelta.y;
+		transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Clamp(zoom, minZ, maxZ));
+	}
+}
---
+++ Assets/Scripts/MouseRotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRotate : MonoBehaviour 
+{
+	public float speed = 1f;
+
+	void Update () 
+	{
+		if (Input.GetMouseButton(0))
+		{
+			transform.Rotate( new  Vector3(Input.GetAxis("Mouse Y"), -Input.GetAxis("Mouse X"), 0) * Time.deltaTime * speed );
+		}
+	}
+}
