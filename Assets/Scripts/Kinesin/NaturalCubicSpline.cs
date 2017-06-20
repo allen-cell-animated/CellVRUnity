7721782f4dad706881346b559cd58161d493cf13
Blair Lyons <blair208@gmail.com>
2017-06-20 10:52:56 -0700

    WIP calculating cubic spline positions and tangents for t

--- Assets/Scripts/Kinesin/NaturalCubicSpline.cs
+++ Assets/Scripts/Kinesin/NaturalCubicSpline.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 namespace AICS.Kinesin
 {
+	[System.Serializable]
+	public class CubicSplinePosition
+	{
+		public int segmentIndex;
+		public float segmentT;
+		public float arcLength;
+
+		public CubicSplinePosition (int _segmentIndex, float _segmentT)
+		{
+			segmentIndex = _segmentIndex;
+			segmentT = _segmentT;
+		}
+	}
+
 	public class NaturalCubicSpline : Spline 
 	{
 		public float segmentLength;
@@ -33,7 +47,7 @@ namespace AICS.Kinesin
 		protected override void UpdateCurve ()
 		{
 			CalculateCurve();
-			EquispaceSegments();
+//			EquispaceSegments();
 		}
 
 		protected override void Draw ()
@@ -69,11 +83,14 @@ namespace AICS.Kinesin
 			}
 		}
 
+		public CubicSplinePosition[] pointPositions;
+
 		void CalculateCurve ()
 		{
 			int divisions = Mathf.FloorToInt( (float) resolution / (float) n );
 			segmentPositions = new Vector3[divisions * (n - 1) + n];
 			tangents = new Vector3[segmentPositions.Length];
+			pointPositions = new CubicSplinePosition[n];
 			linearSystemSolution = new float[3][];
 			for (int axis = 0; axis < 3; axis++)
 			{
@@ -88,6 +105,7 @@ namespace AICS.Kinesin
 				for (int i = 0; i < n - 1; i++)
 				{
 					int segments = (i < n - 2 ? divisions + 1 : divisions + 2);
+					if (axis == 0) { pointPositions[i] = new CubicSplinePosition( k, 0 ); }
 					for (int s = 0; s < segments; s++)
 					{
 						float t = s / (divisions + 1f);
@@ -109,32 +127,48 @@ namespace AICS.Kinesin
 
 		void EquispaceSegments ()
 		{
-			EquispaceSegments( segmentLength );
-		}
-
-		void EquispaceSegments (float spacing)
-		{
-			segmentLength = spacing;
 			List<Vector3> newSegmentPositions = new List<Vector3>();
+			List<Vector3> newTangents = new List<Vector3>();
 			float leftoverDistance = segmentLength;
+			int currentPoint = 0;
+			int k = 0;
 			for (int i = 0; i < segmentPositions.Length - 1; i++)
 			{
+				if (i == 0)
+				{
+					pointPositions[currentPoint] = new CubicSplinePosition( 0, 0 );
+					currentPoint++;
+				}
+				else if (currentPoint != n - 1 && i == pointPositions[currentPoint].segmentIndex)
+				{
+					pointPositions[currentPoint] = new CubicSplinePosition( k - 1, leftoverDistance / segmentLength );
+					currentPoint++;
+				}
+
 				float l = SegmentLength( i );
 				float tInc = segmentLength / l;
 				float t = (segmentLength - leftoverDistance) / l;
 				while (t < 1f)
 				{
-					newSegmentPositions.Add( GetPointOnSegment( i, t ) );
+					newSegmentPositions.Add( Vector3.Lerp( segmentPositions[i], segmentPositions[i + 1], t ) );
+					newTangents.Add( Vector3.Lerp( tangents[i], tangents[i + 1], t ) );
+					k++;
 					t += tInc;
 				}
 				leftoverDistance = (1 - (t - tInc)) * l;
+
+				if (i == segmentPositions.Length - 2)
+				{
+					pointPositions[currentPoint] = new CubicSplinePosition( k - 1, leftoverDistance / segmentLength );
+				}
 			}
 			segmentPositions = newSegmentPositions.ToArray();
+			tangents = newTangents.ToArray();
 		}
 
-		Vector3 GetPointOnSegment (int index, float t)
+		CubicSplinePosition GetSplinePositionForT (float t)
 		{
-			return Vector3.Lerp( segmentPositions[index], segmentPositions[index + 1], t );
+			
 		}
 
 		public override Vector3 GetPoint (float t)
@@ -158,15 +192,21 @@ namespace AICS.Kinesin
 		public override Vector3 GetTangent (float t)
 		{
 			UpdateCurve();
+//			float ct = points[i + 1].position[axis] - linearSystemSolution[axis][i + 1];
+//			float c1t = points[i].position[axis] - linearSystemSolution[axis][i];
+//			Vector3 position = linearSystemSolution[axis][i + 1] * Mathf.Pow( t, 3f ) + linearSystemSolution[axis][i] * Mathf.Pow( 1f - t, 3f ) 
+//				+ ct * t + c1t * (1f - t);
+//			Vector3 tangent = 3f * linearSystemSolution[axis][i + 1] * Mathf.Pow( t, 2f ) - 3f * linearSystemSolution[axis][i] * Mathf.Pow( 1f - t, 2f ) 
+//				+ ct - c1t;
 			return Vector3.zero;
 		}
 
 		public override Vector3[] GetIncrementalPoints (float spacing)
 		{
-			if (segmentLength < spacing - 1f || segmentLength > spacing + 1f)
-			{
-				EquispaceSegments( spacing );
-			}
+//			if (segmentLength < spacing - 1f || segmentLength > spacing + 1f)
+//			{
+//				EquispaceSegments( spacing );
+//			}
 			return segmentPositions;
 		}
 	}
