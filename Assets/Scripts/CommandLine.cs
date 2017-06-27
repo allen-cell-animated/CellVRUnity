c7b35f095098fd644dd250bb89ef373933ebb340
Blair Lyons <blair208@gmail.com>
2017-06-27 15:22:03 -0700

    added test script to open a scene from the command line

---
+++ Assets/Scripts/CommandLine.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class CommandLine 
+{
+	public static void Test ()
+	{
+		Debug.Log( "Hello!" );
+		EditorSceneManager.OpenScene( "Assets/Scenes/necklinker.unity" );
+		EditorApplication.isPlaying = true;
+		Debug.Log( "hips mass = " + GameObject.Find( "Hips").GetComponent<Rigidbody>().mass );
+	}
+}
