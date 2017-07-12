96a877fa889ea1997bb6b64f55bb7e798c2ae69d
Blair Lyons <blair208@gmail.com>
2017-07-12 13:42:11 -0700

    random forces don’t depend on mass, reactivated physics tropomyosin, limiting displacement of organelle per second

---
+++ Assets/Scripts/AICS/Kinesin/PhysicsOrganelle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AICS.Kinesin
+{
+	public class PhysicsOrganelle : MonoBehaviour 
+	{
+		public float maxDisplacementPerSecond = 1f;
+
+		Vector3 lastPosition;
+
+		void Start ()
+		{
+			lastPosition = transform.position;
+		}
+
+		void Update () 
+		{
+			LimitMotion();
+		}
+
+		void LimitMotion ()
+		{
+			Vector3 displacement = transform.position - lastPosition;
+			transform.position = lastPosition + maxDisplacementPerSecond * Time.deltaTime * Vector3.Normalize( displacement );
+			lastPosition = transform.position;
+		}
+	}
+}
