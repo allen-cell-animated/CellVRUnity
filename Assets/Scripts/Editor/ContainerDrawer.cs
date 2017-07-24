28391e61babd37a539e544f25045c6e6b70f8072
Blair Lyons <blair208@gmail.com>
2017-07-24 15:08:04 -0500

    resizable container for testing

---
+++ Assets/Scripts/Editor/ContainerDrawer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor( typeof(Container) )]
+public class ContainerEditor : Editor
+{
+	public override void OnInspectorGUI () 
+	{
+		Container container = (Container)target;
+
+		EditorGUI.BeginChangeCheck();
+
+		container.size = EditorGUILayout.Vector3Field( "Size", container.size );
+		container.wallWidth = EditorGUILayout.FloatField( "Wall width", container.wallWidth );
+
+		if (EditorGUI.EndChangeCheck())
+		{
+			container.SetWalls();
+		}
+	}
+}
