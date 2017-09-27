1987a358c2071ed1af09a5b425d8bfa89827fd2d
Blair Lyons <blair208@gmail.com>
2017-09-27 15:05:54 -0700

    prep for VR build: full tropomyosin, no tubulin flashing, etc

--- Assets/Scripts/AICS/MotorProteins/Kinesin/Motor.cs
+++ Assets/Scripts/AICS/MotorProteins/Kinesin/Motor.cs
@@ -383,7 +383,7 @@ namespace AICS.MotorProteins.Kinesin
 				if (t != null && TubulinIsValid( t ))
 				{
 					molecules.Add( ma );
-					t.Flash( tubulinFlashColor );
+//					t.Flash( tubulinFlashColor );
 				}
 			}
 
