adda31203f5879edc02ca16236a5873b5991ea83
Blair Lyons <blair208@gmail.com>
2017-10-25 13:56:18 -0700

    added floating nucleotides (TODO bind them with kinesin)

--- Assets/Scripts/AICS/PhysicsKinesin/RandomForces.cs
+++ Assets/Scripts/AICS/PhysicsKinesin/RandomForces.cs
@@ -55,7 +55,7 @@ namespace AICS.PhysicsKinesin
 			get {
 				// mass * time interval * multiplier * sqrt( diffusion coefficient * time step (ps) )
 				float meanForce = Mathf.Min( 100f, body.mass ) * timeInterval * 2100f 
-					* Mathf.Sqrt( 20f * 1E-4f * KinesinParameterInput.Instance.dTime.value ); 
+					* Mathf.Sqrt( 20f * 1E-4f * 100000f ); 
 				return Mathf.Log( Random.Range( float.Epsilon, 1f ) ) / (-1f / meanForce); // random exponential distribution
 			}
 		}
@@ -65,7 +65,7 @@ namespace AICS.PhysicsKinesin
 			get {
 				// mass * time interval * multiplier * sqrt( diffusion coefficient * time step (ps) )
 				float meanTorque = Mathf.Min( 100f, body.mass ) * timeInterval * 1500f 
-					* Mathf.Sqrt( 20f * 1E-4f * KinesinParameterInput.Instance.dTime.value ); 
+					* Mathf.Sqrt( 20f * 1E-4f * 100000f ); 
 				return Mathf.Log( Random.Range( float.Epsilon, 1f ) ) / (-1f / meanTorque); // random exponential distribution
 			}
 		}
