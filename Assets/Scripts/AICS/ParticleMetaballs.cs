8084e5be7880b4b8fa69624cbbd295be81f8b1c7
Blair Lyons <blair208@gmail.com>
2017-10-26 15:05:30 -0700

    necklinkers are visualized as metaballs

---
+++ Assets/Scripts/AICS/ParticleMetaballs.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AICS
+{
+	public class ParticleMetaballs : MonoBehaviour 
+	{
+		public Transform[] points;
+		public ParticleSystem emitter;
+
+		ParticleSystem.Particle[] particles;
+
+		void Start () 
+		{
+			EmitParticlePerPoint();
+		}
+
+		void EmitParticlePerPoint ()
+		{
+			ParticleSystem.EmitParams parameters = new ParticleSystem.EmitParams();
+			parameters.velocity = Vector3.zero;
+			parameters.startSize = 1f;
+			parameters.startLifetime = Mathf.Infinity;
+			parameters.startColor = Color.white;
+
+			for (int i = 0; i < points.Length; i++)
+			{
+				parameters.randomSeed = (uint)i;
+				parameters.position = points[i].position;
+				emitter.Emit( parameters, 1 );
+			}
+
+			particles = new ParticleSystem.Particle[points.Length];
+		}
+
+		void Update () 
+		{
+			UpdateParticlePositions();
+		}
+
+		void UpdateParticlePositions ()
+		{
+			emitter.GetParticles( particles );
+			for (int i = 0; i < particles.Length; i++)
+			{
+				particles[i].position = points[particles[i].randomSeed].position;
+			}
+			emitter.SetParticles( particles, particles.Length );
+		}
+	}
+}
