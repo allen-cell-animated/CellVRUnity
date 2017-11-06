d95dd77ab4c76e138c21d415f27ccd6d2ab07092
ALLENINST\blairl <blairl@alleninstitute.org>
2017-11-06 11:44:57 -0800

    put cell on your head to transition to kinesin scene

---
+++ Assets/Scripts/Vive/CellViveHead.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AICS.Cell;
+
+public class CellViveHead : MonoBehaviour
+{
+    public SteamVR_LoadLevel levelLoader;
+
+    void OnTriggerEnter (Collider other)
+    {
+        Cell cell = other.GetComponent<Cell>();
+        if (cell != null)
+        {
+            Debug.Log("entered cell");
+            levelLoader.Trigger();
+        }
+    }
+}
