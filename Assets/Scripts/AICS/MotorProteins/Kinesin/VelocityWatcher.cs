c67480bbba5818a6af8192ec798b18fc6a0e3984
Blair Lyons <blair208@gmail.com>
2017-11-14 16:14:03 -0800

    velocity and acceleration tracking for kinematic rigidbodies (untested)

--- Assets/Scripts/AICS/MotorProteins/Kinesin/VelocityWatcher.cs
+++ Assets/Scripts/AICS/MotorProteins/Kinesin/VelocityWatcher.cs
@@ -6,26 +6,59 @@ namespace AICS.MotorProteins.Kinesin
 {
 	public class VelocityWatcher : MonoBehaviour 
 	{
-		Vector3 currentPosition;
-		Vector3 lastPosition;
+		int n = 4;
+		Vector3[] positions;
 
 		public Vector3 displacement
 		{
 			get
 			{
-				return transform.position - lastPosition;
+				return (positions[n - 1] - positions[0]) / (n - 1f);
+			}
+		}
+
+		public Vector3 velocity
+		{
+			get
+			{
+				Vector3 v = Vector3.zero;
+				for (int i = 0; i < n - 1; i++)
+				{
+					v += positions[i + 1] - positions[i];
+				}
+				return v / (n - 1);
+			}
+		}
+
+		public Vector3 acceleration
+		{
+			get
+			{
+				Vector3 a = Vector3.zero;
+				for (int i = 0; i < n - 2; i++)
+				{
+					a += positions[i + 2] - 2f * positions[i + 1] + positions[i];
+				}
+				return a / (n - 2);
 			}
 		}
 
 		void Start ()
 		{
-			currentPosition = lastPosition = transform.position;
+			positions = new Vector3[n];
+			foreach (Vector3 position in positions)
+			{
+				position = transform.position;
+			}
 		}
 
 		void Update () 
 		{
-			lastPosition = currentPosition;
-			currentPosition = transform.position;
+			for (int i = 0; i < n - 1; i++)
+			{
+				positions[i] = positions[i + 1];
+			}
+			positions[n - 1] = transform.position;
 		}
 	}
 }
