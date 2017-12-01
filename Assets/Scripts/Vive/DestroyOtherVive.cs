c25527468113bb76e4b20116774161d4fc6df63c
Blair Lyons <blair208@gmail.com>
2017-11-30 16:37:45 -0800

    WIP destroy vive rig on load

---
+++ Assets/Scripts/Vive/DestroyOtherVive.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOtherVive : MonoBehaviour 
+{
+	SteamVR_PlayArea playArea;
+
+
+	void Start () 
+	{
+		SteamVR_PlayArea[] vives = GetComponents<SteamVR_PlayArea>();
+		for (int i = 0; i < 2; i++)
+		{
+			if (i > vives.Length - 1)
+			{
+				return;
+			}
+			if (vives[i].gameObject != gameObject)
+			{
+				Destroy( vives[i].gameObject );
+			}
+		}
+	}
+}
