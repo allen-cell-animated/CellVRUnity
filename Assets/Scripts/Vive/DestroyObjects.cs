dfc1fcc49378194c9702d57d9d0ce906ce5683eb
Blair Lyons <blair208@gmail.com>
2017-11-30 16:50:27 -0800

    destroying objects by name

---
+++ Assets/Scripts/Vive/DestroyObjects.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjects : MonoBehaviour 
+{
+	public string[] namesOfObjectsToDestroy;
+
+	void Start () 
+	{
+		for (int i = 0; i < namesOfObjectsToDestroy.Length; i++)
+		{
+			GameObject obj = GameObject.Find( namesOfObjectsToDestroy[i] );
+			if (obj != null)
+			{
+				Debug.Log( "destroying " + obj.name );
+				Destroy( obj );
+			}
+		}
+	}
+}
