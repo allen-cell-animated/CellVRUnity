97a796c071a2a3a52dea927a9cf526ef31275b8a
Blair Lyons <blair208@gmail.com>
2018-08-29 10:18:57 -0700

    3D slider handle, tweaks

---
+++ Assets/Scripts/VisualGuide/SliderHandle3D.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderHandle3D : MonoBehaviour 
+{
+    public Color highlightColor;
+    public GameObject handle3D;
+
+    Color defaultColor;
+
+    Material _material;
+    Material material
+    {
+        get
+        {
+            if (_material == null)
+            {
+                _material = handle3D.GetComponent<MeshRenderer>().material;
+            }
+            return _material;
+        }
+    }
+
+    void Start ()
+    {
+        defaultColor = material.GetColor( "_Color" );
+    }
+
+    void OnMouseEnter ()
+    {
+        material.SetColor( "_Color", highlightColor );
+    }
+
+    void OnMouseExit ()
+    {
+        material.SetColor( "_Color", defaultColor );
+    }
+}
