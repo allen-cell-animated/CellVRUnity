77530bf583e8744e60d2ef1232a3c34af76f8127
Blair Lyons <blair208@gmail.com>
2018-10-25 14:19:59 -0700

    mitotic cells are spawned and dropped with physics

--- Assets/Scripts/VisualGuide/MitosisGameManager.cs
+++ Assets/Scripts/VisualGuide/MitosisGameManager.cs
@@ -2,15 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MitosisGameManager : MonoBehaviour {
+public class MitosisGameManager : MonoBehaviour 
+{
+    public bool inPlayMode;
+    public string currentStructureName;
+    public Bounds throwableSpawnArea;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    string[] throwableNames = {"ProphaseCell", "PrometaphaseCell", "MetaphaseCell", "AnaphaseCell", "TelophaseCell"};
+
+    void Start ()
+    {
+        StartCoroutine( "SpawnThrowables" );
+    }
+
+    void OnDrawGizmos ()
+    {
+        Gizmos.DrawWireCube( throwableSpawnArea.center, throwableSpawnArea.size );
+    }
+
+    Vector3 randomPositionInThrowableSpawnArea
+    {
+        get
+        {
+            return new Vector3( Random.Range( throwableSpawnArea.min.x, throwableSpawnArea.max.x ),
+                                Random.Range( throwableSpawnArea.min.y, throwableSpawnArea.max.y ),
+                                Random.Range( throwableSpawnArea.min.z, throwableSpawnArea.max.z ));
+        }
+    }
+
+    IEnumerator SpawnThrowables ()
+    {
+        GameObject prefab;
+        foreach (string t in throwableNames)
+        {
+            prefab = Resources.Load( currentStructureName + "/" + t ) as GameObject;
+            if (prefab == null)
+            {
+                Debug.LogWarning( "Couldn't load prefab for " + currentStructureName + " " + t );
+                continue;
+            }
+
+            Instantiate( prefab, randomPositionInThrowableSpawnArea, Random.rotation );
+
+            yield return new WaitForSeconds( Random.Range( 0.05f, 0.3f ) );
+        }
+
+        yield return null;
+    }
 }
