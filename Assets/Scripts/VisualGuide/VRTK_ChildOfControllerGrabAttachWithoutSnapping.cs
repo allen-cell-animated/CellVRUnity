98ccf3e76f4ff186cc21033eef35ee0fb1a7e52b
Blair Lyons <blair208@gmail.com>
2018-11-12 10:26:30 -0800

    overriding VRTK component to not snap object to controller when picked up

---
+++ Assets/Scripts/VisualGuide/VRTK_ChildOfControllerGrabAttachWithoutSnapping.cs
@@ -0,0 +1,13 @@
+﻿namespace VRTK.GrabAttachMechanics
+{
+    using UnityEngine;
+
+    [AddComponentMenu("VRTK/Scripts/Interactions/Interactables/Grab Attach Mechanics/VRTK_ChildOfControllerGrabAttachWithoutSnapping")]
+    public class VRTK_ChildOfControllerGrabAttachWithoutSnapping : VRTK_ChildOfControllerGrabAttach
+    {
+        protected override void SnapObjectToGrabToController(GameObject obj)
+        {
+            obj.transform.SetParent(controllerAttachPoint.transform);
+        }
+    }
+}
