60fc64a81613015838e12908754365d212dbdd61
Blair Lyons <blair208@gmail.com>
2018-11-30 14:55:54 -0800

    collapsible info panel

---
+++ Assets/Scripts/VisualGuide/CollapsiblePanel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollapsiblePanel : MonoBehaviour 
+{
+    public GameObject hotspot;
+    public Animator panel;
+
+    List<Collider> collidingControllers = new List<Collider>();
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.tag == "GameController")
+        {
+            if (collidingControllers.Count < 1)
+            {
+                SetPanel( true );
+            }
+            collidingControllers.Add( other );
+        }
+    }
+
+    void OnTriggerExit (Collider other)
+    {
+        if (collidingControllers.Contains( other ))
+        {
+            collidingControllers.Remove( other );
+            if (collidingControllers.Count < 1)
+            {
+                SetPanel( false );
+            }
+        }
+    }
+
+    void SetPanel (bool open)
+    {
+        panel.SetTrigger( open ? "Open" : "Close" );
+        hotspot.SetActive( !open );
+    }
+}
