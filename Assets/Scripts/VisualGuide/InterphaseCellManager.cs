9a7d7fbccae550383db5cccc93b701797727499d
Blair Lyons <blair208@gmail.com>
2018-12-03 14:10:43 -0800

    bug fix

--- Assets/Scripts/VisualGuide/InterphaseCellManager.cs
+++ Assets/Scripts/VisualGuide/InterphaseCellManager.cs
@@ -140,7 +140,7 @@ public class InterphaseCellManager : MonoBehaviour
 
     public void ColorActiveStructure ()
     {
-        structures.Find( s => s.gameObject.activeSelf ).colorer.SetColor( 1 );
+        structures.Find( s => s.gameObject.activeSelf && s.canSelect ).colorer.SetColor( 1 );
     }
 
     public void TransitionToLobbyMode ()
