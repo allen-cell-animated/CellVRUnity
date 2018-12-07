caee171fc7bd119b3dc7c424c56cfad77f529e07
Blair Lyons <blair208@gmail.com>
2018-12-07 10:58:03 -0800

    countdown canvas smaller and in front of head

--- Assets/Scripts/VisualGuide/InfoCanvas.cs
+++ Assets/Scripts/VisualGuide/InfoCanvas.cs
@@ -9,39 +9,10 @@ public class InfoCanvas : MonoBehaviour
     public Image image;
     public Text text;
 
-    Vector3 offsetFromCamera = new Vector3( -1f, 0, 1f );
-
-    Transform _calculator;
-    Transform calculator
-    {
-        get
-        {
-            if (_calculator == null)
-            {
-                _calculator = new GameObject( "Calculator" ).transform;
-            }
-            return _calculator;
-        }
-    }
-
     public void SetContent (StructureData _data)
     {
         title.text = _data.structureName;
         image.sprite = _data.infoImage;
         text.text = _data.description;
-
-        //SetPosition();
-    }
-
-    void SetPosition ()
-    {
-        calculator.position = Camera.main.transform.position;
-        calculator.rotation = Quaternion.Euler( 0, Camera.main.transform.rotation.eulerAngles.y, Camera.main.transform.rotation.eulerAngles.z );
-
-        Vector3 position = calculator.TransformPoint( offsetFromCamera );
-        transform.position = new Vector3( position.x, 1f, position.z );
-
-        transform.LookAt( transform.position + (transform.position - Camera.main.transform.position) );
-        transform.rotation = Quaternion.Euler( 0, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z );
     }
 }
